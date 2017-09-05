facc8c84460784082bd820d05aaf273d5c99a715
Salil Shenoy <salil@Salils-MacBook-Pro.local>
2017-09-05 13:46:29 -0700

    Issue: Modified the PoS tagging algorithm

--- hindi_tagger.php
+++ hindi_tagger.php
@@ -8,6 +8,12 @@ $tags = ['NN', 'VB', 'CONJ', 'POST_POS', 'JJ', 'RB', 'OTHER','Q'];
 
 class HindiTokenizer
 {
+   public static function removePunctuations($text) 
+   {
+	//$text = "है।";
+	 return preg_replace('/(|)\p{P}/u', ' ', $text); 
+   }
+
    public static function tagTokenizePartsofSpeech($text)
    {
        static $dictionary = [];
@@ -23,8 +29,6 @@ class HindiTokenizer
            gzclose($fh);
        }
        preg_match_all("/[\w\d]+/", $text, $matches);
-       $nouns = ['NN','NNP','NNS'];
-       $verbs = ['VBZ','VBD','VBN'];
        $tokens = explode(' ', $text);
 
        $result = [];
@@ -32,93 +36,95 @@ class HindiTokenizer
        $i = 0;
 
        foreach ($tokens as $token) {
- 
           //Tag the tokens as found in the Lexicon
           $current = ['token' => $token, 'tag' => 'UNKNOWN'];
-          if (!empty($dictionary[$token])) {
-               $tag_list = $dictionary[$token];
-               $current['tag'] = $tag_list[0];
-           }
+          if (is_numeric($token)) {
+	            $current['tag'] = "Q";
+	        } elseif (!empty($dictionary[$token])) {
+              $tag_list = $dictionary[$token];
+              if ($tag_list[0] == "case") {
+                $tag_list[0] = "CONJ";
+              }
+              $current['tag'] = $tag_list[0];
+          }
 
-           //NOUN IDENTIFICATION
+	        $result[$i] = $current;
+          $i++;
+	     }
        
-           //RULE 1: If the previous word tagged is a Adjective / Pronoun / Postposition then 
-           //the current word is likely to be a noun
-           if ($previous['tag'] == 'JJ'     || 
-               $previous['tag'] == 'PRO_NN' ||
-               $previous['tag'] == 'POST_POS') {
-               $current['tag'] = 'NN';
-           }
-
-           //RULE 2: If the current word is a verb then the previous word is likely to be a 
-           //noun
-           if (in_array($current['tag'], $verbs)) {
-               $previous['tag'] = 'NN';
-               $result[$i-1] = $previous;
-           }
-
-           //RULE 3: If the current tag is a noun then next / previous is likely to be a noun
-
-           //DEMONSTRATIVE IDENTIFICATION
-
-           //RULE 1: If the current and previous words are tagged as pronouns then the previous
-           //word is likley to be a demonstrative
-           if ($current['tag'] == 'PRO_NN' && 
-               $previous['tag'] == 'PRO_NN') {
-               $previous['tag'] = 'DEM';
-               $result[$i-1] = $previous;
-           }
-
-           //RULE 2: If current word is a noun and previous word is a pronoun then the current
-           //word is liklely to be demonstrative
-           if ($current['tag'] == 'NN' && 
-               $previous['tag'] == 'PRO_NN') {
-               $current['tag'] = 'DEM';
-           }
-
-           //PRONOUN IDENTIFICATION
-
-           //RULE 1: If the previous word is unknown and cuurent word is a noun then the previous
-           //word is most likely to be a pronoun
-           if ($previous['tag'] == 'UNKNOWN' && 
-               $current['tag'] == 'NN') {
-               $previous['tag'] = 'PRO_NN';
-               $result[$i-1] = $previous;
-           }
-
-           //NAME Identification
-
-           //RULE: If we get two words which are untagged the most probably they form a name and 
-           //will be tagged as noun
-           if ($previous['tag'] == 'UNKNOWN' && 
-               $current['tag'] == 'UNKNOWN') {
-               $current['tag'] = 'NN';
-               $previous['tag'] = 'NN';
-               $result[$i-1] = $previous;
-           }
-
-           //ADJECTIVE IDENTIFCATION
-
-           //RULE: If the word ends with <tar>, <tam>, <thik> then we tag it as a Adjective
-       
-
-           //VERB IDENTIFICATION
-
-           //RULE: If the current word is tagged as Auxilary verb  and previous word is tagged as 
-           //Unknown then most likely that the previous word is a verb
-           if ($current['tag'] == 'VAUX' &&
-               $previous['tag'] == 'UNKNOWN') {
-               $previous['tag'] = 'VB';
-               $result[$i-1] = $previous;
-           }
-   
-           $result[$i] = $current;
-           $i++;
-           $previous = $current;
-           $previous_token = $token; 
-       }
-
        return $result;
+   }
+
+   public static function tagUnknownWords($partiallyTaggedText) 
+   {
+     $result = $partiallyTaggedText;
+     $nouns = ['NN','NNP','NNS'];
+     $verbs = ['VBZ','VBD','VBN'];
+	   $length = count($result);
+	   $previous = $result[0];
+	   for ($i = 1; $i < $length; $i++)
+	   {
+	      $current = $result[$i];
+        print("Line No.: " + $i);
+        print("\t");
+        print($current['token']);
+        print("\t");
+        print($current['tag']);
+        print("\t");
+        print($previous['token']);
+        print("\t");
+        print($previous['tag']);
+        print("\n");
+        if ($current['tag'] == "UNKNOWN" || $previous['tag'] == "UNKNOWN") {
+              
+              //RULE 1: If the previous word tagged is a Adjective / Pronoun / Postposition then
+              //the current word is likely to be a noun
+              if ($previous['tag'] == 'JJ'     ||
+                  $previous['tag'] == 'PRO_NN' ||
+                  $previous['tag'] == 'POST_POS') 
+              {
+                  $current['tag'] = 'NN';
+                  $result[$i] = $current;
+              }
+              //RULE 2: If the current word is a verb then the previous word is likely to be a
+              //noun
+              elseif (in_array($current['tag'], $verbs)) {
+                $previous['tag'] = 'NN';
+                $result[$i] = $previous;
+              }
+              //PRONOUN IDENTIFICATION
+              //RULE 1: If the previous word is unknown and cuurent word is a noun then the previous
+              //word is most likely to be a pronoun
+              elseif ($previous['tag'] == 'UNKNOWN' &&
+                $current['tag'] == 'NN') {
+                $previous['tag'] = 'PRO_NN';
+                $result[$i] = $previous;
+              }
+              //VERB IDENTIFICATION
+              //RULE: If the current word is tagged as Auxilary verb  and previous word is tagged as
+              //Unknown then most likely that the previous word is a verb
+              elseif ($current['tag'] == 'VAUX' &&
+                $previous['tag'] == 'UNKNOWN') {
+                $previous['tag'] = 'VB';
+                $result[$i] = $previous;
+              }
+              else 
+              {
+                if ($current['tag'] == "UNKNOWN") {
+                  $current['tag'] = 'NN';
+                  $result[$i] = $current;
+                } else {
+                  $previous['tag'] = 'NN';
+                  $result[$i-1] = $previous;
+                }
+                
+              }
+          }
+
+          $previous = $current;
+      }
+
+      return $result;
    }
 
    public static function taggedPartOfSpeechTokensToString($tagged_tokens, 
@@ -150,13 +156,13 @@ class HindiTokenizer
 
 $hiToken = new HindiTokenizer;
 $text = "श्रीनगर में एक 200 साऱ पुरानी दरगाह में आग ऱगनेके बाद प्रदशशनकाररयों नेपुलऱस पर पथराव ककया है और इऱाके में तनाव है।"; 
-$tagged_tokens = $hiToken->tagTokenizePartsofSpeech($text);
-#print_r ($tagged_tokens);
+$text = $hiToken->removePunctuations($text);
 
+$tagged_tokens = $hiToken->tagTokenizePartsofSpeech($text);
+$tagged_tokens = $hiToken->tagUnknownWords($tagged_tokens);
 $tagged_phrase = $hiToken->taggedPartOfSpeechTokensToString(
                            $tagged_tokens);
-print ($tagged_phrase);
-
+print($tagged_phrase);
 #श्रीनगर मेंएक 200 साऱ पुरानी दरगाह मेंआग ऱगनेके बाद प्रदशशनकाररयों नेपुलऱस पर पथराव ककया है और इऱाके मेंतनाव है।
 ?>
 
